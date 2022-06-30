8965261e445d7ba74a1e2b7b14fbc9ebb6325ec5
Oussama Khiar <okhiar@e2r1p12.1337.ma>
2022-06-30 15:11:38 +0100

    Makefile && Libraries

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okhiar <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 16:51:02 by okhiar            #+#    #+#             */
+/*   Updated: 2022/06/22 16:56:05 by okhiar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	check_sep(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_strs(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && check_sep(str[i], charset))
+			i++;
+		if (str[i] != '\0')
+			count++;
+		while (str[i] != '\0' && !check_sep(str[i], charset))
+			i++;
+	}
+	return (count);
+}
+
+int	ft_strlen_sep(char *str, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && !check_sep(str[i], charset))
+		i++;
+	return (i);
+}
+
+char	*ft_word(char *str, char *charset)
+{
+	int		len_word;
+	int		i;
+	char	*word;
+
+	i = 0;
+	len_word = ft_strlen_sep(str, charset);
+	word = (char *)malloc(sizeof(char) * (len_word + 1));
+	while (i < len_word)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**strings;
+	int		i;
+
+	i = 0;
+	strings = (char **)malloc(sizeof(char *) * (count_strs(str, charset) + 1));
+	while (*str)
+	{
+		while (*str && check_sep(*str, charset))
+			str++;
+		if (*str)
+		{
+			strings[i] = ft_word(str, charset);
+			i++;
+		}
+		while (*str && !check_sep(*str, charset))
+			str++;
+	}
+	strings[i] = 0;
+	return (strings);
+}
